92ef5ebde44dc085d6474c0690b900103ec38048
Anurag <hamsen3880@gmail.com>
2024-01-21 13:24:51 +0530

    new lecture #3

---
+++ Lecture 03/models/todos/todo.models.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const todoSchema = new mongoose.Schema({
+    content: {
+        type: String,
+        required: true
+    },
+    complete: {
+        type: Boolean,
+        default: false
+    },
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+    subTodos: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Sub-Todo"
+        }
+    ] // Array of sub-todos
+}, {timestamps: true})
+
+export const User = mongoose.model("Todo", todoSchema)
